69303ac6b42d1b339e5ca42a3a290ff36c75825b
LuizHAC <luizh5391@gmail.com>
2021-10-24 04:12:14 -0300

    Refactoring classes and creating the movement

--- Pages/Game_Page/main.js
+++ Pages/Game_Page/main.js
@@ -1,193 +1,135 @@
 // Creating the Entity class
-
 class Entity {
-
-    constructor( { tag = 'div', className = '' } = {} ) {
-        this.el = document.createElement();
-        this.tag = 'div';
-        document.body.appendChild(this.el);
-        this.el.className = className;
+    constructor({ tag = 'div', className = '' } = {}) {
+      this.el = document.createElement(tag);
+      document.body.appendChild(this.el);
+      this.el.className = 'entity ' + className;
+      this.el.style.position = 'absolute';
     }
-
-    setPosition(x, y) {
+  
+    setPosition(x, y){
         this.x = x;
         this.y = y;
 
-        this.el.style.left = `${this.x}px`;
+        this.el.style.left = `${this.x}px`
+        this.el.style.position = 'absolute';
         this.el.style.top = `${this.y}px`;
     }
-
-    removeEntity(){
-        this.el.remove();
-        this.el = null;
+  
+    remove() {
+      this.el.remove();
+      this.el = null;
     }
-}
+  }
 
-// Creating a class to control the Lives
-
-class Lives extends Entity {
-
+  // Creating the ship class
+class Ship  extends Entity{
     constructor() {
-        super();
-        this.lives = 3;
-        this.setPosition(window.innerWidth / 2, window.innerHeight - 20);
-        this.loadText();
+        super({className: 'ship', tag: 'img'});
+        this.el.src = '../../Assets/Game/ship.png';
+
+        this.speed = 5;
+        this.canShot = true;
+        this.el.className = 'ship';
+        this.setPosition(window.innerWidth / 2, window.innerHeight - 120)
     }
 
-    removeLife(){
-        this.lives = this.lives - 1;
-        this.loadText();
+    moveRight(){
+        this.setPosition(this.x + this.speed, this.y);
     }
 
-    loadText() {
-        this.el.innerText = this.lives;
-    }
-}
-
-// Creating a class to control the Scores
-
-class Score extends Entity{
-
-    constructor(){
-        super();
-        this.score = 0;
-        this.setPosition(window.innerWidth / 2, 20);
-        this.loadText();
+    moveLeft(){
+        this.setPosition(this.x - this.speed, this.y);
     }
 
-    addScore(amount) {
-        this.score = this.score + amount;
-        this.loadText();
-    }
+    shot({createBullet}){
+        
+        if(this.canShot){
+            this.canShot = false;
+            createBullet({x: this.x + 43.5, y: this.y - 20});
+            setTimeout( () => {
+                this.canShot = true;
+            }, 500);
+        }
 
-    loadText(){
-        this.el.innerText = `Score: ${this.score}`;
     }
 }
 
-// Creating the Bullets class
+class Bullet extends Entity{
+    constructor({x, y}){
+        super({className: 'bullet'});
 
-class Bullet extends Entity {
-    constructor(x, y, isAlien) {
-        this.speed = 3;
-        this.isAlien = isAlien;
+        this.speed = 5;
+        this.el.className = 'bullet';
+        this.el.style.backgroundColor = 'white';
+        this.el.style.width = `${5}px`;
+        this.el.style.height = `${15}px`;
 
         this.setPosition(x, y);
     }
 
-    update() {
-        const bullet_y = this.isAlien ? this.speed : -this.speed;
-        this.setPosition(this.x, this.y + bullet_y);
+    update(){
+        this.setPosition(this.x, this.y - this.speed);
     }
 }
 
-// Creating the Aliens class
+const KEY_RIGHT = 39;
+const KEY_LEFT = 37;
+const KEY_SPACE = 32
 
-class Alien extends Entity {
+const keys = {
+    right: false,
+    left: false,
+    space:false
+}
 
-    constructor(x, y, type){
-        this.x = x;
-        this.y = y;
-        this.score = 0;
-    }
-
-    setImage(type){
-        if(type == 1){
-            this.el.src = '../../Assets/Game/enemy1.png';
-        }
-        else if(type == 2){
-            this.el.src = '../../Assets/Game/enemy2.png';
-        }
-        else if(type == 3){
-            this.el.src = '../../Assets/Game/enemy3.png';
-        }
-        else{
-            this.el.src = '../../Assets/Game/enemy4.png';
-        }
+// Key Presses
+function KeyPress(event) {
+    if (event.keyCode === KEY_RIGHT) {
+      keys.right = true;
+      console.log(keys)
+    } else if (event.keyCode === KEY_LEFT) {
+        keys.left = true;
+    } else if (event.keyCode === KEY_SPACE) {
+        keys.space = true;
     }
 }
 
-
-class Ship {
-    constructor(removeLife, getOverlappingBullet, removeBullet, type) {
-        this.type = type;
-        this.el.src = '../../Assets/Game/ship.png';
-        document.body.appendChild(this.el);
-
-        this.spped = 2;
-        this.image_width = 50;
-        this.canFire = true;
-        this.isAlive = true;
-
-        this.removeLife = removeLife;
-        this.getOverlappingBullet = getOverlappingBullet;
-        this.removeBullet = removeBullet;
-
-        this.spawn();
-    }
-
-    setColor(type){
-        if(type == 1){
-            this.color = 'red';
-        }
-        else{
-            this.color = 'blue';
-        }
-
-    }
-
-    spawn(){
-        this.isAlive = true;
-        this.el.style.opacity = 1;
-        this.setPosition(window.innerWidth / 2, window.innerHeight - 80);
-    }
-
-    moveRight() {
-        if (this.isAlive == true){
-            this.setPosition(this.x + this.speed, this.y);
-        }
-        else{
-            return;
-        }
-    }
-
-    moveLeft() {
-        if (this.isAlive == true){
-            this.setPosition(this.x - this.speed, this.y);
-        }
-        else{
-            return;
-        }
-    }
-
-    fire(createBullet) {
-        if(this.canFire && this.isAlive) {
-            this.canFire = false;
-            createBullet(this.x + this.image_width / 2, this.y);
-        }
-
-        setTimeout( () => {
-            this.canFire = true;
-        }, 1000)
-    }
-
-    kill () {
-        this.isAlive = false;
-
-        setTimeout( () => {
-            this.spawn();
-        }, 3000);
-
-        this.el.style.opacity = 0;
-    }
-
-    update() {
-        const bullet = this.getOverlappingBullet(this);
-        if(bullet && bullet.isAlien && this.isAlive) {
-            this.removeBullet(bullet);
-            this.removeLife();
-            this.kill();
-        }
+// Key Releases
+function KeyRelease(event) {
+    if (event.keyCode === KEY_RIGHT) {
+        keys.right = false;
+    } else if (event.keyCode === KEY_LEFT) {
+        keys.left = false;
+    } else if (event.keyCode === KEY_SPACE) {
+        keys.space = false;
     }
 }
 
+window.addEventListener("keydown", KeyPress);
+window.addEventListener("keyup", KeyRelease);
+
+const ship = new Ship();
+const bullets = [];
+
+const createBullet = ({x, y}) => {
+    bullets.push(new Bullet({x: ship.x + 43.5, y: ship.y - 20}));
+}
+
+const update = () => {
+    if (keys.right && ship.x < window.innerWidth - 100){
+        ship.moveRight();
+    }
+    else if (keys.left && ship.x > 0){
+        ship.moveLeft();
+    }
+
+    if (keys.space){
+        // Create a bullet
+        ship.shot({createBullet})
+    }
+
+    bullets.forEach(bullet => bullet.update());
+}
+
+setInterval(update, 0.2);
