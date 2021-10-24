54163618da90d0dd54039366b554cff15e7f6c54
LuizHAC <luizh5391@gmail.com>
2021-10-23 23:33:12 -0300

    Added other classes to the game

--- Pages/Game_Page/main.js
+++ Pages/Game_Page/main.js
@@ -64,3 +64,130 @@ class Score extends Entity{
         this.el.innerText = `Score: ${this.score}`;
     }
 }
+
+// Creating the Bullets class
+
+class Bullet extends Entity {
+    constructor(x, y, isAlien) {
+        this.speed = 3;
+        this.isAlien = isAlien;
+
+        this.setPosition(x, y);
+    }
+
+    update() {
+        const bullet_y = this.isAlien ? this.speed : -this.speed;
+        this.setPosition(this.x, this.y + bullet_y);
+    }
+}
+
+// Creating the Aliens class
+
+class Alien extends Entity {
+
+    constructor(x, y, type){
+        this.x = x;
+        this.y = y;
+        this.score = 0;
+    }
+
+    setImage(type){
+        if(type == 1){
+            this.el.src = '../../Assets/Game/enemy1.png';
+        }
+        else if(type == 2){
+            this.el.src = '../../Assets/Game/enemy2.png';
+        }
+        else if(type == 3){
+            this.el.src = '../../Assets/Game/enemy3.png';
+        }
+        else{
+            this.el.src = '../../Assets/Game/enemy4.png';
+        }
+    }
+}
+
+
+class Ship {
+    constructor(removeLife, getOverlappingBullet, removeBullet, type) {
+        this.type = type;
+        this.el.src = '../../Assets/Game/ship.png';
+        document.body.appendChild(this.el);
+
+        this.spped = 2;
+        this.image_width = 50;
+        this.canFire = true;
+        this.isAlive = true;
+
+        this.removeLife = removeLife;
+        this.getOverlappingBullet = getOverlappingBullet;
+        this.removeBullet = removeBullet;
+
+        this.spawn();
+    }
+
+    setColor(type){
+        if(type == 1){
+            this.color = 'red';
+        }
+        else{
+            this.color = 'blue';
+        }
+
+    }
+
+    spawn(){
+        this.isAlive = true;
+        this.el.style.opacity = 1;
+        this.setPosition(window.innerWidth / 2, window.innerHeight - 80);
+    }
+
+    moveRight() {
+        if (this.isAlive == true){
+            this.setPosition(this.x + this.speed, this.y);
+        }
+        else{
+            return;
+        }
+    }
+
+    moveLeft() {
+        if (this.isAlive == true){
+            this.setPosition(this.x - this.speed, this.y);
+        }
+        else{
+            return;
+        }
+    }
+
+    fire(createBullet) {
+        if(this.canFire && this.isAlive) {
+            this.canFire = false;
+            createBullet(this.x + this.image_width / 2, this.y);
+        }
+
+        setTimeout( () => {
+            this.canFire = true;
+        }, 1000)
+    }
+
+    kill () {
+        this.isAlive = false;
+
+        setTimeout( () => {
+            this.spawn();
+        }, 3000);
+
+        this.el.style.opacity = 0;
+    }
+
+    update() {
+        const bullet = this.getOverlappingBullet(this);
+        if(bullet && bullet.isAlien && this.isAlive) {
+            this.removeBullet(bullet);
+            this.removeLife();
+            this.kill();
+        }
+    }
+}
+
