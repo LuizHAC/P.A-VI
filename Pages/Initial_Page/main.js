e0d33f79f7dff98f833b0d4b628e681442748d2c
JuhMei <JuhMei@users.noreply.github.com>
2021-12-01 01:32:53 -0300

    setting

--- Pages/Initial_Page/main.js
+++ Pages/Initial_Page/main.js
@@ -1,12 +1,12 @@
 // Navigates to the second page
 function playGame(){
-
     setTimeout( () => window.location.href = "../Game_Page/index.html", 1000);
 }
 
 // Play music
 function playMusic(){
     var myMusic = document.getElementById("music")
+    myMusic.volume = 0.6; //max 1.0
     myMusic.play();
 }
 
