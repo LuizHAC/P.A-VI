d39a2db9c6d1a82be7d6e5e62f70e7c567740d16
JuhMei <JuhMei@users.noreply.github.com>
2021-11-30 23:42:43 -0300

    menu setting

--- Pages/Settings_Page/main.js
+++ Pages/Settings_Page/main.js
@@ -1,4 +1,10 @@
 // Loads the menu
 function loadMenu(){
-    setTimeout( () => window.location.href = "../Menu/index.html", 1000);
+    setTimeout( () => window.location.href = "../Initial_Page/index.html", 1000);
 }
+
+function togglePlay() {
+    var myMusic = document.getElementById("music")
+    myMusic.volume = 0.6; //max 1.0
+    return myMusic.paused ? myMusic.play() : myMusic.pause();
+};
